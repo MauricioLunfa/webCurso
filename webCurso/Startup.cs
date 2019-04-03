7f0e91ec75ebf265363b052d170e4e766db39785
Mauricio Barreto <mauriciobarretom@hotmail.com>
2019-04-03 17:16:08 -0300

    segunda testes

--- webCurso/Startup.cs
+++ webCurso/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using webCurso.Models;
 
 namespace webCurso
 {
@@ -33,6 +35,9 @@ namespace webCurso
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<webCursoContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("webCursoContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
